ddee67331d09e6265d4cfe43e83ddbf717ff44da
HidetoNiwa <tototohide@gmail.com>
2019-09-12 20:50:29 +0900

    raspi camera gray&white

--- raspi/openCV.py
+++ raspi/openCV.py
@@ -1,12 +1,34 @@
+#coding: UTF-8
 import cv2
+import numpy
+import sys
 
-CAP = cv2.VideoCapture(0)  # 0はカメラのデバイス番号
+cap = cv2.VideoCapture(0)  # 0はカメラのデバイス番号
+
+tm = cv2.TickMeter()
+tm.start()
+
+count = 0
+max_count = 10
+fps = 0
 
 while(True):
-    FRAME = CAP.read()
-    cv2.imshow('frame', FRAME)
+    ret,frame = cap.read()
+
+    if count == max_count:
+	tm.stop()
+	fps = max_count/tm.getTimeSec()
+	tm.reset()
+	tm.start()
+	count=0
+
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    ret,frame = cv2.threshold(frame,0,255,cv2.THRESH_OTSU)
+    print("fps:{:.1f}".format(fps))
+    cv2.imshow('frame',frame)
+    count += 1
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-CAP.release()
+cap.release()
 cv2.destroyAllWindows()
