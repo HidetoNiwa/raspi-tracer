8fafbdb12b2371639eda7694bf705ce5e6cd9c4d
HidetoNiwa <tototohide@gmail.com>
2019-09-26 12:11:06 +0900

    とりあえずライントレースできるようになった、あとはパラメータ調整

--- RasberryPi/src/main.cpp
+++ RasberryPi/src/main.cpp
@@ -1,4 +1,6 @@
 #include "main.h"
+using namespace cv;
+using namespace std;
 
 //motorクラスの定義
 motor w[4]{
@@ -8,7 +10,7 @@ motor w[4]{
     motor(3)
 };
 
-void vector(int x,int y ,int theta);
+void vectors(int x,int y ,int theta);
 
 int main(int argh, char* argv[])
 {
@@ -22,7 +24,7 @@ int main(int argh, char* argv[])
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
 
     //motor初期状態定義
-    //vector(0,0,0);
+    vectors(0,0,0);
 
     cv::Mat frame; //取得したフレーム
     uint16_t def_y=480/(PLOTS+1);
@@ -30,34 +32,57 @@ int main(int argh, char* argv[])
     {
 
         linearApprox lin;
-
-        cv::cvtColor(frame, frame,cv::COLOR_RGB2GRAY);
-        cv::threshold(frame, frame, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
-
-        uint16_t y=def_y;
-        uint16_t l[PLOTS]={0};
-        uint16_t r[PLOTS]={0};
+	GaussianBlur(frame,frame,Size(3,3),0);
+        cvtColor(frame, frame,cv::COLOR_RGB2GRAY);
+        //adaptiveThreshold(frame,frame, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 7, 8);
+	threshold(frame, frame, 0, 255, THRESH_BINARY | THRESH_OTSU);
+	/*
+	int threshold_type = atoi(argv[2]) ?THRESH_BINARY : THRESH_BINARY_INV;
+	int adaptive_method = atoi(argv[3]) ?ADAPTIVE_THRESH_MEAN_C : ADAPTIVE_THRESH_GAUSSIAN_C;
+	int block_size =atoi (argv[4]);
+	double offset = (double)atof(argv[5]);
+	adaptiveThreshold(frame, frame, 255, adaptive_method,threshold_type, block_size, offset);
+	*/
+	uint16_t y=def_y;
+        uint16_t l[PLOTS]={640};
+        uint16_t r[PLOTS]={640};
         uint16_t centor[PLOTS]={0};
 
         for(uint8_t i=0;i<PLOTS;i++){
-            for(uint16_t x=0;x<640;x++){
-                if(255 == frame.at<unsigned char>(y, x)){
-                    l[i]=x;
+	    for(uint16_t x=0;x<640;x++){
+                if(0 != frame.at<unsigned char>(y, x)){
+                    uint8_t count=0;
+		    for(uint8_t j=0;j<21;j++){
+		    	if(0 != frame.at<unsigned char>(y, x+j)){
+				count++;
+			}
+		    }
+		    if(count>20){
+		    	l[i]=x;
+		    }
+                    break;
+            	}
+            }
+            for(uint16_t x=639;x>l[i]+50;x--){
+                if(0 != frame.at<unsigned char>(y, x)){
+                    uint8_t count=0;
+                    for(uint8_t j=0;j<21;j++){
+                        if(0 != frame.at<unsigned char>(y, x-j)){
+                                count++;
+                        }
+                    }
+                    if(count>20){
+                        r[i]=x;
+                    }
                     break;
                 }
-            }
-            for(uint16_t x=l[0];x<640;x++){
-                if(0 == frame.at<unsigned char>(y, x)){
-                    r[i]=x;
-                    break;
-                }
-            }
-            if(r[i]==0){
-                r[i]=640;
             }
             y=y+def_y;
             centor[i]=(l[i]+r[i])/2;
-            lin.add(y,centor[i]);
+	    if((l[i]<630)&&(r[i]>10)){
+            	lin.add(y,centor[i]);
+		circle(frame, Point(centor[i],y), 20, Scalar(200,200,200), 1, 8);
+	    }
             //printf("%d,%d\t",y,centor[i]);
         }
 	float b=lin.getB();
@@ -65,15 +90,19 @@ int main(int argh, char* argv[])
         printf("y=%fx+%f\t%d\n",a,b,lin.getN());
 	float dify=lin.getB()-CENTOR_B;
 	float outY=dify*P_Y;
-	float outTHETA=a*P_THETA;
+	float outTHETA=a*a*P_THETA;
+	if(outTHETA>14){
+		outTHETA=14;
+	}
 	float outX;
 	if((outY>2)||(outTHETA>2)){
-		outX=0;
+		outX=2;
 	}else{
-		outX=4;
+		outX=8;
 	}
         //vector(7,(int)outY*1.5,(int)outTHETA+outY);
-	vector(outX,(int)outY,(int)(outTHETA-(outY*2)));
+	vectors(outX,(int)outY,(int)(outTHETA));
+	cv::line(frame, cv::Point(b,0), cv::Point(480*a+b, 480), cv::Scalar(0,0,0), 3, 4);
 	cv::imshow("Lune",frame);
 /*
 	for(uint8_t i=0;i<4;i++){
@@ -89,6 +118,7 @@ int main(int argh, char* argv[])
         const int key = cv::waitKey(1);
         if(key == 'q'/*113*/)//qボタンが押されたとき
         {
+	    vectors(0,0,0);
             break;//whileループから抜ける．
         }
         else if(key == 's'/*115*/)//sが押されたとき
@@ -102,7 +132,7 @@ int main(int argh, char* argv[])
     return 0;
 }
 
-void vector(int x,int y ,int theta){
+void vectors(int x,int y ,int theta){
     //theta=-theta;
     x=-x;
     int vector[4]={0};
--- RasberryPi/src/main.h
+++ RasberryPi/src/main.h
@@ -6,5 +6,5 @@
 #define MAX_POWER 100
 #define PLOTS 15
 #define CENTOR_B 320
-#define P_Y 0.03
-#define P_THETA 25
+#define P_Y 0.06
+#define P_THETA -20
